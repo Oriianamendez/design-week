0869bff7162246a614c689fa25531d2c060d2cc1
Oriana Mendez <egleemendez@gmail.com>
2024-08-05 10:31:38 +0200

    gh-11 main page skeleton

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,48 @@
 import styles from "./page.module.css";
 
 export default function Home() {
-  return <main className={styles.main}></main>;
+  return (
+    <>
+      <header>
+        <nav>
+          <p>Logo</p>
+          <p>Login</p>
+        </nav>
+      </header>
+      <main className={styles.main}>
+        <h1>Welcome to ReadQuest</h1>
+        <p>This is a description for ReadQuest</p>
+
+        <article>
+          <section>
+            <h2>Book name</h2>
+            <p>Description of the book</p>
+            <p>Points</p>
+          </section>
+          <section>
+            <h2>Book name</h2>
+            <p>Description of the book</p>
+            <p>Points</p>
+          </section>
+          <section>
+            <h2>Book name</h2>
+            <p>Description of the book</p>
+            <p>Points</p>
+          </section>
+        </article>
+
+        <form action="">
+          <label>Book</label>
+          <input type="text" />
+          <label htmlFor="">Question</label>
+          <input type="text" />
+          <label htmlFor="">Question</label>
+          <input type="text" />
+          <label htmlFor="">Question</label>
+          <input type="text" />
+          <button>Get your points</button>
+        </form>
+      </main>
+    </>
+  );
 }
